954ba5455a4fd4184b064996fca35a8c940a2926
Tadas Maksimavičius <tadasmak@gmail.com>
2026-01-10 12:07:01 +0200

    Create AddFolder and AddFile methods in VirtualFolder class

--- Saldo_homework/Domain/Folder.cs
+++ Saldo_homework/Domain/Folder.cs
@@ -10,4 +10,24 @@ public class VirtualFolder
         Subfolders = new Dictionary<string, VirtualFolder>();
         Files = new Dictionary<string, VirtualFile>();
     }
+
+    public void AddFolder(string name)
+    {
+        if (Subfolders.ContainsKey(name))
+        {
+            throw new InvalidOperationException("Folder already exists");
+        }
+
+        Subfolders[name] = new VirtualFolder(name);
+    }
+
+    public void AddFile(VirtualFile file)
+    {
+        if (Files.ContainsKey(file.Name))
+        {
+            throw new InvalidOperationException("File already exists");
+        }
+
+        Files[file.name] = file;
+    }
 }
