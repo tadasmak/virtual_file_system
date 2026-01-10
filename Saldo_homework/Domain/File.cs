c9c89b4f1e670d66530208b9115317ee923b0e0a
Tadas Maksimavičius <tadasmak@gmail.com>
2026-01-10 11:59:09 +0200

    Create File class

---
+++ Saldo_homework/Domain/File.cs
@@ -0,0 +1,11 @@
+public class File
+{
+    public string Name { get; }
+    public string SourcePath { get; }
+    
+    public File(string name, string sourcePath)
+    {
+        Name = name;
+        SourcePath = sourcePath;
+    }
+}
