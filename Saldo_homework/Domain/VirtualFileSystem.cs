e7dc5697daa2a4c1abda3bc7f2a8339ba520b2d4
Tadas Maksimavičius <tadasmak@gmail.com>
2026-01-10 14:15:36 +0200

    Add GetFolder method into VirtualFileSystem class

--- Saldo_homework/Domain/VirtualFileSystem.cs
+++ Saldo_homework/Domain/VirtualFileSystem.cs
@@ -8,5 +8,28 @@ namespace SaldoHomework.Domain
         {
             Root = new VirtualFolder("/");
         }
+
+        public VirtualFolder GetFolder(string path)
+        {
+            if (path == "/")
+            {
+                return Root;
+            }
+
+            var parts = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+            var current = Root;
+
+            foreach (var part in parts)
+            {
+                if (!current.Subfolders.TryGetValue(part, out var next))
+                {
+                    throw new InvalidOperationException("Invalid path");
+                }
+
+                current = next;
+            }
+
+            return current;
+        }
     }
 }
