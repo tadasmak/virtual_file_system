e06376930f0a9916090d2ae26bf711ef7e726783
Tadas Maksimavičius <tadasmak@gmail.com>
2026-01-11 14:09:09 +0200

    Implement VirtualFileSystem.GetFolder() tests

--- Saldo_homework.Tests/VirtualFileSystemTests.cs
+++ Saldo_homework.Tests/VirtualFileSystemTests.cs
@@ -6,12 +6,41 @@ namespace SaldoHomework.Tests
     public class VirtualFileSystemTests
     {
         [Fact]
-        public void ConstructorCreatesRootFolder()
+        public void Constructor_CreatesRootFolder()
         {
             var vfs = new VirtualFileSystem();
 
             Assert.NotNull(vfs.Root);
             Assert.Equal("/", vfs.Root.Name);
+        }
+
+        [Fact]
+        public void GetFolder_RootPath_ReturnsRoot()
+        {
+            var vfs = new VirtualFileSystem();
+            var folder = vfs.GetFolder("/");
+
+            Assert.Equal("/", folder.Name);
+        }
+
+        [Fact]
+        public void GetFolder_ValidPath_ReturnsCorrectFolder()
+        {
+            var vfs = new VirtualFileSystem();
+            vfs.Root.AddFolder("documents");
+            vfs.Root.Subfolders["documents"].AddFolder("work");
+
+            var folder = vfs.GetFolder("/documents/work");
+
+            Assert.Equal("work", folder.Name);
+        }
+
+        [Fact]
+        public void GetFolder_InvalidPath_ThrowsException()
+        {
+            var vfs = new VirtualFileSystem();
+
+            Assert.Throws<InvalidOperationException>(() => vfs.GetFolder("/nonexistent"));
         }
     }
 }
